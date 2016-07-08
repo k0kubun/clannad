20522e564c656928add0b0048dc26bc03260b12f
Takashi Kokubun <takashikkbn@gmail.com>
2016-07-08 23:57:54 +0900

    Refactor to drop builder from argument

--- src/compiler.c
+++ src/compiler.c
@@ -7,6 +7,7 @@
 typedef struct {
   LLVMModuleRef mod;
   LLVMValueRef func;
+  LLVMBuilderRef builder;
   Dict *syms; /* Symbol table for compiling scope */
 } Compiler;
 
@@ -30,14 +31,14 @@ compile_int(Node *node)
 }
 
 LLVMValueRef
-compile_string(LLVMBuilderRef builder, Node *node)
+compile_string(Node *node)
 {
   assert_node(node, NODE_STRING);
-  return LLVMBuildGlobalStringPtr(builder, node->id, "");
+  return LLVMBuildGlobalStringPtr(compiler.builder, node->id, "");
 }
 
 LLVMValueRef
-compile_variable(LLVMBuilderRef builder, Node *node)
+compile_variable(Node *node)
 {
   assert_node(node, NODE_IDENTIFIER);
 
@@ -46,19 +47,19 @@ compile_variable(LLVMBuilderRef builder, Node *node)
     fprintf(stderr, "Undefined variable: %s\n", node->id);
     exit(1);
   }
-  return LLVMBuildLoad(builder, var, node->id);
+  return LLVMBuildLoad(compiler.builder, var, node->id);
 }
 
-LLVMValueRef compile_exp(LLVMBuilderRef builder, Node *node);
+LLVMValueRef compile_exp(Node *node);
 
 void
-compile_return(LLVMBuilderRef builder, Node *node)
+compile_return(Node *node)
 {
   assert_node(node, NODE_RETURN);
   if (node->param) {
-    LLVMBuildRet(builder, compile_exp(builder, node->param));
+    LLVMBuildRet(compiler.builder, compile_exp(node->param));
   } else {
-    LLVMBuildRetVoid(builder);
+    LLVMBuildRetVoid(compiler.builder);
   }
 }
 
@@ -80,7 +81,7 @@ compile_type(Node *node)
 }
 
 LLVMValueRef
-compile_unary(LLVMBuilderRef builder, Node *node)
+compile_unary(Node *node)
 {
   Node *target;
   if (node->lhs) target = node->lhs;
@@ -90,20 +91,20 @@ compile_unary(LLVMBuilderRef builder, Node *node)
     return LLVMConstIntCast(LLVMSizeOf(compile_type(target)), LLVMInt32Type(), 0);
 
   LLVMValueRef var;
-  LLVMValueRef result = compile_exp(builder, target);
+  LLVMValueRef result = compile_exp(target);
   switch ((int)node->op) {
     case INC_OP:
       // FIXME: maybe some assertion required
       var = dict_get(compiler.syms, target->id);
-      LLVMBuildStore(builder, LLVMBuildAdd(builder, result, LLVMConstInt(LLVMInt32Type(), 1, 0), ""), var);
+      LLVMBuildStore(compiler.builder, LLVMBuildAdd(compiler.builder, result, LLVMConstInt(LLVMInt32Type(), 1, 0), ""), var);
       break;
     case DEC_OP:
       // FIXME: maybe some assertion required
       var = dict_get(compiler.syms, target->id);
-      LLVMBuildStore(builder, LLVMBuildSub(builder, result, LLVMConstInt(LLVMInt32Type(), 1, 0), ""), var);
+      LLVMBuildStore(compiler.builder, LLVMBuildSub(compiler.builder, result, LLVMConstInt(LLVMInt32Type(), 1, 0), ""), var);
       break;
     case '!':
-      result = LLVMBuildICmp(builder, LLVMIntEQ, result, LLVMConstInt(LLVMTypeOf(result), 0, 0), "");
+      result = LLVMBuildICmp(compiler.builder, LLVMIntEQ, result, LLVMConstInt(LLVMTypeOf(result), 0, 0), "");
       break;
     default:
       fprintf(stderr, "Unexpected operator in compile_unary: %d\n", (int)node->op);
@@ -115,55 +116,55 @@ compile_unary(LLVMBuilderRef builder, Node *node)
 }
 
 LLVMValueRef
-compile_binop(LLVMBuilderRef builder, Node *node)
+compile_binop(Node *node)
 {
   assert_node(node, NODE_BINOP);
 
   LLVMValueRef var;
   switch (node->op) {
     case '+':
-      return LLVMBuildAdd(builder, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildAdd(compiler.builder, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case '-':
-      return LLVMBuildSub(builder, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildSub(compiler.builder, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case '*':
-      return LLVMBuildMul(builder, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildMul(compiler.builder, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case '/':
-      return LLVMBuildSDiv(builder, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildSDiv(compiler.builder, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case '%':
-      return LLVMBuildSRem(builder, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildSRem(compiler.builder, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case '=':
       var = dict_get(compiler.syms, node->lhs->id);
       if (var == NULL) {
         fprintf(stderr, "Undefined variable: %s\n", node->lhs->id);
         exit(1);
       }
-      return LLVMBuildStore(builder, compile_exp(builder, node->rhs), var);
+      return LLVMBuildStore(compiler.builder, compile_exp(node->rhs), var);
     case '<':
-      return LLVMBuildICmp(builder, LLVMIntSLT, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildICmp(compiler.builder, LLVMIntSLT, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case '>':
-      return LLVMBuildICmp(builder, LLVMIntSGT, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildICmp(compiler.builder, LLVMIntSGT, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case '&':
-      return LLVMBuildAnd(builder, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildAnd(compiler.builder, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case '|':
-      return LLVMBuildOr(builder, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildOr(compiler.builder, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case '^':
-      return LLVMBuildXor(builder, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildXor(compiler.builder, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case LE_OP:
-      return LLVMBuildICmp(builder, LLVMIntSLE, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildICmp(compiler.builder, LLVMIntSLE, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case GE_OP:
-      return LLVMBuildICmp(builder, LLVMIntSGE, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildICmp(compiler.builder, LLVMIntSGE, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case EQ_OP:
-      return LLVMBuildICmp(builder, LLVMIntEQ, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildICmp(compiler.builder, LLVMIntEQ, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case NE_OP:
-      return LLVMBuildICmp(builder, LLVMIntNE, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildICmp(compiler.builder, LLVMIntNE, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case AND_OP:
-      return LLVMBuildBinOp(builder, LLVMAnd, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildBinOp(compiler.builder, LLVMAnd, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case OR_OP:
-      return LLVMBuildBinOp(builder, LLVMOr, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildBinOp(compiler.builder, LLVMOr, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case LEFT_OP:
-      return LLVMBuildBinOp(builder, LLVMShl, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildBinOp(compiler.builder, LLVMShl, compile_exp(node->lhs), compile_exp(node->rhs), "");
     case RIGHT_OP:
-      return LLVMBuildBinOp(builder, LLVMLShr, compile_exp(builder, node->lhs), compile_exp(builder, node->rhs), "");
+      return LLVMBuildBinOp(compiler.builder, LLVMLShr, compile_exp(node->lhs), compile_exp(node->rhs), "");
     default:
       fprintf(stderr, "Unexpected binary operation: %c\n", node->op);
       exit(1);
@@ -171,7 +172,7 @@ compile_binop(LLVMBuilderRef builder, Node *node)
 }
 
 LLVMValueRef
-compile_funcall(LLVMBuilderRef builder, Node *node)
+compile_funcall(Node *node)
 {
   assert_node(node, NODE_FUNCALL);
 
@@ -181,28 +182,28 @@ compile_funcall(LLVMBuilderRef builder, Node *node)
   // Build arguments
   for (int i = 0; i < node->params->length; i++) {
     Node *param = (Node *)vector_get(node->params, i);
-    args[i] = compile_exp(builder, param);
+    args[i] = compile_exp(param);
   }
 
-  return LLVMBuildCall(builder, func, args, node->params->length, "");
+  return LLVMBuildCall(compiler.builder, func, args, node->params->length, "");
 }
 
 LLVMValueRef
-compile_exp(LLVMBuilderRef builder, Node *node)
+compile_exp(Node *node)
 {
   switch (node->kind) {
     case NODE_UNARY:
-      return compile_unary(builder, node);
+      return compile_unary(node);
     case NODE_BINOP:
-      return compile_binop(builder, node);
+      return compile_binop(node);
     case NODE_INTEGER:
       return compile_int(node);
     case NODE_STRING:
-      return compile_string(builder, node);
+      return compile_string(node);
     case NODE_IDENTIFIER:
-      return compile_variable(builder, node);
+      return compile_variable(node);
     case NODE_FUNCALL:
-      return compile_funcall(builder, node);
+      return compile_funcall(node);
     default:
       fprintf(stderr, "Unexpected node in compile_exp: %s\n", kind_label(node->kind));
       exit(1);
@@ -210,41 +211,41 @@ compile_exp(LLVMBuilderRef builder, Node *node)
 }
 
 void
-compile_var_decl(LLVMBuilderRef builder, Node *node)
+compile_var_decl(Node *node)
 {
   assert_node(node, NODE_VAR_DECL);
 
-  LLVMValueRef var = LLVMBuildAlloca(builder, compile_type(node->type), node->spec->id);
+  LLVMValueRef var = LLVMBuildAlloca(compiler.builder, compile_type(node->type), node->spec->id);
   dict_set(compiler.syms, node->spec->id, var);
 }
 
-void compile_stmt(LLVMBuilderRef builder, Node *node);
+void compile_stmt(Node *node);
 
 void
-compile_if(LLVMBuilderRef builder, Node *node)
+compile_if(Node *node)
 {
   LLVMBasicBlockRef if_block   = LLVMAppendBasicBlock(compiler.func, "if");
   LLVMBasicBlockRef else_block = LLVMAppendBasicBlock(compiler.func, "else");
   LLVMBasicBlockRef end_block  = LLVMAppendBasicBlock(compiler.func, "end");
 
-  LLVMValueRef cond_exp = compile_exp(builder, node->cond);
-  LLVMValueRef cond = LLVMBuildICmp(builder, LLVMIntNE,
+  LLVMValueRef cond_exp = compile_exp(node->cond);
+  LLVMValueRef cond = LLVMBuildICmp(compiler.builder, LLVMIntNE,
       cond_exp, LLVMConstInt(LLVMTypeOf(cond_exp), 0, 0), "");
-  LLVMBuildCondBr(builder, cond, if_block, else_block);
+  LLVMBuildCondBr(compiler.builder, cond, if_block, else_block);
 
-  LLVMPositionBuilderAtEnd(builder, if_block);
-  compile_stmt(builder, node->if_stmt);
-  LLVMBuildBr(builder, end_block);
+  LLVMPositionBuilderAtEnd(compiler.builder, if_block);
+  compile_stmt(node->if_stmt);
+  LLVMBuildBr(compiler.builder, end_block);
 
-  LLVMPositionBuilderAtEnd(builder, else_block);
-  if (node->else_stmt) compile_stmt(builder, node->else_stmt);
-  LLVMBuildBr(builder, end_block);
+  LLVMPositionBuilderAtEnd(compiler.builder, else_block);
+  if (node->else_stmt) compile_stmt(node->else_stmt);
+  LLVMBuildBr(compiler.builder, end_block);
 
-  LLVMPositionBuilderAtEnd(builder, end_block);
+  LLVMPositionBuilderAtEnd(compiler.builder, end_block);
 }
 
 void
-compile_stmt(LLVMBuilderRef builder, Node *node)
+compile_stmt(Node *node)
 {
   switch (node->kind) {
     case NODE_BINOP:
@@ -253,16 +254,16 @@ compile_stmt(LLVMBuilderRef builder, Node *node)
     case NODE_IDENTIFIER:
     case NODE_FUNCALL:
     case NODE_UNARY:
-      compile_exp(builder, node);
+      compile_exp(node);
       break;
     case NODE_VAR_DECL:
-      compile_var_decl(builder, node);
+      compile_var_decl(node);
       break;
     case NODE_IF:
-      compile_if(builder, node);
+      compile_if(node);
       break;
     case NODE_RETURN:
-      compile_return(builder, node);
+      compile_return(node);
       break;
     default:
       fprintf(stderr, "Unexpected node kind in compile_stmt: %s\n", kind_label(node->kind));
@@ -271,13 +272,13 @@ compile_stmt(LLVMBuilderRef builder, Node *node)
 }
 
 void
-compile_comp_stmt(LLVMBuilderRef builder, Node *node)
+compile_comp_stmt(Node *node)
 {
   assert_node(node, NODE_COMPOUND_STMT);
 
   for (int i = 0; i < node->children->length; i++) {
     Node *child = (Node *)vector_get(node->children, i);
-    compile_stmt(builder, child);
+    compile_stmt(child);
   }
 }
 
@@ -327,18 +328,19 @@ compile_func(Node *node)
   // build block instructions
   LLVMBasicBlockRef block = LLVMAppendBasicBlock(compiler.func, "entry");
   LLVMBuilderRef builder = LLVMCreateBuilder();
-  LLVMPositionBuilderAtEnd(builder, block);
+  compiler.builder = builder;
+  LLVMPositionBuilderAtEnd(compiler.builder, block);
 
   // store argument
   for (int i = 0; i < node->spec->params->length; i++) {
     Node *param = vector_get(node->spec->params, i);
     LLVMValueRef val = dict_get(compiler.syms, param->spec->id);
-    LLVMValueRef var = LLVMBuildAlloca(builder, compile_type(param->type), param->spec->id);
+    LLVMValueRef var = LLVMBuildAlloca(compiler.builder, compile_type(param->type), param->spec->id);
     dict_set(compiler.syms, param->spec->id, var);
-    LLVMBuildStore(builder, val, var);
+    LLVMBuildStore(compiler.builder, val, var);
   }
 
-  compile_comp_stmt(builder, node->stmts);
+  compile_comp_stmt(node->stmts);
 }
 
 void
