e0127967fe2ae2a26cfcf0efcdd8f6da9eb06e11
Takashi Kokubun <takashikkbn@gmail.com>
2016-07-18 20:12:51 +0900

    Don't initialize lexer state on comment

--- test/comment.c
+++ test/comment.c
@@ -25,8 +25,16 @@ void test_c99_comment() {
 
 // int test() { exit(1); }
 
+void test_comment_should_not_rewind_state() {
+# if 0
+  /* */
+  expect_int(1, 0);
+# endif
+}
+
 int main() {
   test_c89_comment();
   test_c99_comment();
+  test_comment_should_not_rewind_state();
   return 0;
 }
